bcb5ac3a1107b0afe43ce2213655bed05cde310c
benjamin.indermuehle <benjamin.indermuehle@students.fhnw.ch>
2014-11-01 17:14:47 +0100

    added uri handling for windows

--- src/library/PropertyFile.java
+++ src/library/PropertyFile.java
@@ -4,7 +4,9 @@ package library;
 import java.util.Properties;
 import java.io.*;
 import java.net.InetAddress;
+import java.net.URISyntaxException;
 import java.net.UnknownHostException;
+import java.nio.file.Paths;
 
 import server.Server;
 
@@ -21,13 +23,18 @@ public class PropertyFile extends Properties{
 	
 	public PropertyFile() throws IOException{
 		super();
-		String myLocation = Server.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-		String configFile = myLocation+ PropertyFile.propFileName ;
-		this.inputStream =  new FileInputStream(configFile);
-		this.load(this.inputStream);
-        if (inputStream == null) {
-            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-        }
+		try {
+			String myLocation = Paths.get(Server.class.getProtectionDomain().getCodeSource().getLocation().toURI()).toString();
+			String configFile = myLocation+ PropertyFile.propFileName ;	
+			this.inputStream =  new FileInputStream(configFile);
+			this.load(this.inputStream);
+	        if (inputStream == null) {
+	            throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
+	        }
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 	public InetAddress getServerAddress() throws UnknownHostException {
